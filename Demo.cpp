2d4760a486dabb18aef913ca74b4801a4b53eb2b
Fiwpr <htfi597@gmail.com>
2025-02-24 14:45:51 +0700

    thirt commit

--- Demo.cpp
+++ Demo.cpp
@@ -8,5 +8,8 @@ int main(){
     for (int a = 0; a < n; a++)
         for (int b = 0; b < n; b++)
             cout << "Hello !" << endl;
+    for (int a = 0; a < n; a++)
+        for (int b = 0; b < n; b++)
+            cout << "Hello !" << endl;
     return 0;
 }
